c12bccf3460e0167780c6b8826b842eb15844c6d
Mia_Chen <1094574940@qq.com>
2020-06-05 19:52:56 +0800

    dist commit

--- src/swiper/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-
-// require styles
-import 'swiper/dist/css/swiper.css'
-
-Vue.use(VueAwesomeSwiper)
-
-export default VueAwesomeSwiper
